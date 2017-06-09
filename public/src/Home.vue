0ce5310d138cb66276bfca0231234473fb5760bb
vince lynch <root@vps307308.ovh.net>
2017-06-09 11:45:25 +0100

    MEVN Mongo Express VueJS Node - Hackathon boilerplate

---
+++ public/src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
---
+++ public/src/components/Home.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div id="intro">
+	<h1>Music App</h1>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+	name: 'intro',
+	data () {
+		return {
+		  msg: 'Welcome to Your Vue.js App'
+		}
+	},
+	mounted() {
+	  // When the application loads, we want to call the method that initializes
+	  // some data
+	  //return 
+	  console.log("[home -> vue.ready()]")
+	  this.fetchEvents();
+
+
+	},
+	methods: {
+		fetchEvents: function(){
+			console.log('fetchEvents called');
+		}
+	}
+}
+</script>
